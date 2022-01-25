0a26d25553a41eae9b6616c05344b17548dac3ca
Brandon <brandon.franco@nivelics.co>
2022-01-25 14:45:21 -0500

    axtualización de repositorío

--- themes/src/app/components/articulos/articulos.component.ts
+++ themes/src/app/components/articulos/articulos.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-articulos',
   templateUrl: './articulos.component.html',
-  styleUrls: ['./articulos.component.css']
+  styleUrls: ['./articulos.component.scss']
 })
 export class ArticulosComponent implements OnInit {
 
