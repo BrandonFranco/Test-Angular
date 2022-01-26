d60551559c043861d323c8504086ec816a37cfca
Brandon <brandon.franco@nivelics.co>
2022-01-26 11:42:11 -0500

    axtualización de repositorío

--- themes/src/assets/js/custom.js
+++
@@ -1,6 +0,0 @@
-jQuery(document).ready(function() {
-    $('span.icon-menu').click(function() {
-    $(this).toggleClass('active');
-    $(this).parent().find('.container-header').first().slideToggle();
-    });
-});
