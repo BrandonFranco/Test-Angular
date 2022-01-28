0eccfb40b706c95cdcc0166468a7afe212b3d794
Brandon <brandon.franco@nivelics.co>
2022-01-27 19:53:02 -0500

    repositorio

--- themes/src/app/app.module.ts
+++ themes/src/app/app.module.ts
@@ -2,6 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MatButtonModule } from '@angular/material/button';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {LayoutModule} from '@angular/cdk/layout';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -46,14 +51,21 @@ import { MasLeidoComponent } from './components/mas-leido/mas-leido.component';
     RoboticaComponent,
     MercadeoComponent,
     ArticulosComponent,
-    MasLeidoComponent
+    MasLeidoComponent,
 
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatButtonModule
+    MatButtonModule,
+    MatSidenavModule,
+    MatListModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatIconModule
+
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
--- themes/src/app/components/header/header.component.ts
+++ themes/src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { MediaMatcher } from '@angular/cdk/layout';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
  
 @Component({
   selector: 'app-header',
@@ -7,7 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  mobileQuery: MediaQueryList;
+
+  //fillerNav = Array.from({length: 50}, (_, i) => `Nav Item ${i + 1}`);//
+  fillerNav=[
+    {name:"home", route:"home", icon:"home"},
+    {name:"categorías", route:"products"},
+    {name:"eventos", route:"contact"},
+    {name:"ediciones", route:"ediciones"}
+  ]
+
+  private _mobileQueryListener: () => void;
+
+  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
+  }
+
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
+  }
+
+  shouldRun = true;
+
 
   ngOnInit(): void {
   }
